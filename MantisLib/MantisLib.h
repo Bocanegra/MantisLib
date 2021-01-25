043d9b1e50e2b7be9550fddf86e5478815e0cf11
Luis Ángel García Muñoz <luisg.bocanegra@gmail.com>
2021-01-25 15:40:38 +0100

    Initial project setup

---
+++ MantisLib/MantisLib.h
@@ -0,0 +1,18 @@
+//
+//  MantisLib.h
+//  MantisLib
+//
+//  Created by Luis Ángel García on 25/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MantisLib.
+FOUNDATION_EXPORT double MantisLibVersionNumber;
+
+//! Project version string for MantisLib.
+FOUNDATION_EXPORT const unsigned char MantisLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MantisLib/PublicHeader.h>
+
+
